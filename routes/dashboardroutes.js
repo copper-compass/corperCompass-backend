148a6c5ecd26403d478524f4b6f03547bf83b58a
Justus Okoro <jusreal2@gmail.com>
2026-03-07 09:13:17 +0100

    Delete routes/dashboardroutes.js

--- routes/dashboardroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
-const { getJourney, updateChecklist } = require('../controllers/dashboardController');
-
-router.get('/journey', protect, getJourney);
-router.patch('/checklist/:id', protect, updateChecklist);
-
-module.exports = router;
