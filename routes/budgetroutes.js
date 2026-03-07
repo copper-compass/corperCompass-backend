7e2359a9f7c89089503e2a137c04c8c91702b7c6
Justus Okoro <jusreal2@gmail.com>
2026-03-07 09:12:52 +0100

    Delete routes/budgetroutes.js

--- routes/budgetroutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { calculateBudget } = require('../controllers/budgetController');
-
-router.post('/calculate', calculateBudget);
-
-module.exports = router;
