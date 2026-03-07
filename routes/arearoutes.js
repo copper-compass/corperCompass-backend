cbbcae76ce9832a97e66b38af7eb035179024788
Justus Okoro <jusreal2@gmail.com>
2026-03-07 09:12:26 +0100

    Delete routes/arearoutes.js

--- routes/arearoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAreas, getAreasByState } = require('../controllers/areaController');
-
-router.get('/', getAreas);
-router.get('/:state', getAreasByState);
-
-module.exports = router;
