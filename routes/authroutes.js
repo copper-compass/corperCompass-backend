2aa5b93be417a7990a2f2dedeedefbcbc943c7c7
Justus Okoro <jusreal2@gmail.com>
2026-03-07 09:12:38 +0100

    Delete routes/authroutes.js

--- routes/authroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { register, login } = require('../controllers/authController');
-const { validate, registerValidation, loginValidation } = require('../middleware/validationMiddleware');
-
-router.post('/register', validate(registerValidation), register);
-router.post('/login', validate(loginValidation), login);
-
-module.exports = router;
