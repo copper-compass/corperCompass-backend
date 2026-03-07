a33c5b61cf2e369bc7a5b036acedc4e0641d19a5
Justus Okoro <jusreal2@gmail.com>
2026-03-07 09:12:13 +0100

    Delete routes/Lodgeroutes.js

--- routes/Lodgeroutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getLodges } = require('../controllers/lodgeController');
-
-router.get('/', getLodges);
-
-module.exports = router;
