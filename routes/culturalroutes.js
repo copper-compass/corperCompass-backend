ad6abdbc596ade6bf3787d1d74cc9fb252a079a9
Justus Okoro <jusreal2@gmail.com>
2026-03-07 09:13:04 +0100

    Delete routes/culturalroutes.js

--- routes/culturalroutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCulture } = require('../controllers/cultureController');
-
-router.get('/', getCulture);
-
-module.exports = router;
