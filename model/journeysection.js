3f191feb6df028ac09383999c8fc061237ffaee0
Justus Okoro <jusreal2@gmail.com>
2026-03-07 09:14:47 +0100

    Delete model/journeysection.js

--- model/journeysection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const journeySectionSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  order: { type: Number, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('JourneySection', journeySectionSchema);
