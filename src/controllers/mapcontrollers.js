fe8b60cd8d9c37f38b0e1ac59f815e7fe429511e
Justus Okoro <jusreal2@gmail.com>
2026-03-12 19:47:38 +0100

    Add map controllers for markers and heatmap data

---
+++ src/controllers/mapcontrollers.js
@@ -0,0 +1,65 @@
+import Area from '../models/Area.js';
+import Lodge from '../models/Lodge.js';
+
+// @desc    Get all map markers (areas and lodges with coordinates)
+// @route   GET /api/map/markers
+export const getMapMarkers = async (req, res, next) => {
+  try {
+    const areas = await Area.find({ 'location.coordinates': { $exists: true, $ne: [] } })
+      .select('name state location rentRange');
+    const lodges = await Lodge.find({ 'location.coordinates': { $exists: true, $ne: [] } })
+      .populate('area', 'name state')
+      .select('name area location priceRange');
+
+    const markers = [
+      ...areas.map(area => ({
+        id: area._id,
+        type: 'area',
+        name: area.name,
+        state: area.state,
+        rentRange: area.rentRange,
+        lat: area.location.coordinates[1],
+        lng: area.location.coordinates[0],
+      })),
+      ...lodges.map(lodge => ({
+        id: lodge._id,
+        type: 'lodge',
+        name: lodge.name,
+        area: lodge.area?.name,
+        state: lodge.area?.state,
+        priceRange: lodge.priceRange,
+        lat: lodge.location.coordinates[1],
+        lng: lodge.location.coordinates[0],
+      })),
+    ];
+
+    res.json(markers);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @desc    Get heatmap data (e.g., rent prices)
+// @route   GET /api/map/heatmap
+export const getHeatmapData = async (req, res, next) => {
+  try {
+    const lodges = await Lodge.find({ 'location.coordinates': { $exists: true, $ne: [] } })
+      .populate('area')
+      .select('location priceRange area');
+
+    const heatmapPoints = lodges.map(lodge => {
+      const avgRent = lodge.priceRange?.min && lodge.priceRange?.max
+        ? (lodge.priceRange.min + lodge.priceRange.max) / 2
+        : 50000; // default weight
+      return {
+        lat: lodge.location.coordinates[1],
+        lng: lodge.location.coordinates[0],
+        weight: avgRent / 1000, // scale down for heatmap intensity
+      };
+    });
+
+    res.json(heatmapPoints);
+  } catch (error) {
+    next(error);
+  }
+};
