1bb97af628ac76d79394afb10004a2c35b7faf14
Justus Okoro <jusreal2@gmail.com>
2026-03-12 19:55:47 +0100

    Refactor lodge schema for improved readability

--- src/models/lodge.js
+++ src/models/lodge.js
@@ -2,28 +2,31 @@ import mongoose from 'mongoose';
 
 const lodgeSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    area: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Area',
-      required: true,
-    },
+    name: { type: String, required: true },
+    area: { type: mongoose.Schema.Types.ObjectId, ref: 'Area', required: true },
     priceRange: {
       min: Number,
       max: Number,
     },
     publicContact: String,
     notes: String,
-    isActive: {
-      type: Boolean,
-      default: true,
+    isActive: { type: Boolean, default: true },
+    location: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        default: 'Point',
+      },
+      coordinates: {
+        type: [Number], // [longitude, latitude]
+        required: false,
+      },
     },
   },
   { timestamps: true }
 );
+
+lodgeSchema.index({ location: '2dsphere' });
 
 const Lodge = mongoose.model('Lodge', lodgeSchema);
 export default Lodge;
