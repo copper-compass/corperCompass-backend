134651b143cca366d7906f55c3cf261db6b02643
Justus Okoro <jusreal2@gmail.com>
2026-03-19 11:03:00 +0100

    Refactor user registration and login to include token
    Signed-off-by: Justus Okoro <jusreal2@gmail.com>

--- backend/src/controllers/authcontroller.js
+++ backend/src/controllers/authcontroller.js
@@ -1,48 +1,38 @@
 import User from '../models/User.js';
 import generateToken from '../utils/generateToken.js';
 
-// @desc    Register a new user
-// @route   POST /api/auth/register
 export const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
-
     const userExists = await User.findOne({ email });
     if (userExists) {
       res.status(400);
       throw new Error('User already exists');
     }
     const user = await User.create({ name, email, password });
-    if (user) {
-      generateToken(res, user._id)
-      res.status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role,
-      });
-    } else {
-      res.status(400);
-      throw new Error('Invalid user data');
-    }
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
   } catch (error) {
     next(error);
   }
 };
 
-// @desc    Login user
-// @route   POST /api/auth/login
 export const loginUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (user && (await user.matchPassword(password))) {
-      generateToken(res, user._id)
       res.json({
         _id: user._id,
         name: user.name,
         email: user.email,
         role: user.role,
+        token: generateToken(user._id),
       });
     } else {
       res.status(401);
@@ -53,8 +43,6 @@ export const loginUser = async (req, res, next) => {
   }
 };
 
-// @desc    Get current user profile
-// @route   GET /api/auth/me
 export const getMe = async (req, res, next) => {
   try {
     const user = await User.findById(req.user._id).select('-password');
