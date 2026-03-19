d706d7bc631b1a2f3f3066057040eeb509b4acd0
Justus Okoro <jusreal2@gmail.com>
2026-03-19 10:30:22 +0100

    Refactor state schema for brevity
    Signed-off-by: Justus Okoro <jusreal2@gmail.com>

--- backend/src/models/State.js
+++ backend/src/models/State.js
@@ -1,14 +1,8 @@
 import mongoose from 'mongoose';
 
 const stateSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  region: {
-    type: String,
-  },
+  name: { type: String, required: true, unique: true },
+  region: String,
 });
 
 const State = mongoose.model('State', stateSchema);
