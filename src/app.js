49d242ae75ee32af7a52365c0932d732274c38ce
Omoju-Mayowa <omojumayowa@gmail.com>
2026-03-24 13:10:34 +0100

    fix: made some last minutes tweaks

--- src/app.js
+++ src/app.js
@@ -41,7 +41,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
     'http://localhost:8000',
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    'https://corper-compass-frontend.vercel.app'
 ];
 app.use(cors({
     origin: (origin, callback) => {
